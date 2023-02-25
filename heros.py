3c623d47032af033e44b1e25e074bf2b96b6dc39
RandomDreamWalker 97062 @ENamnor <104535753+PSPFRDW97062@users.noreply.github.com>
2023-02-25 15:31:22 -0600

    Hero Update: Rank 1 Skill All Damage & All Heal

--- heros.py
+++ heros.py
@@ -17,15 +17,15 @@ class Hero:
     enemy_decision = ""
 
     def __init__(self, name, health, attack, level=1, base_attack_stat=0,
-                 rank_2_skill_info: dict = False, gray_skill_info: dict = False):
+                 rank_2_skill_info: dict = False, rank_1_skill_info: dict = False):
         self.name = name
         self.max_health = health
         self.health = health
         self.attack = attack
         self.level = level
         self.base_attack_stat = self.ATTACK_STATS[base_attack_stat]
-        self.green_skill_info = rank_2_skill_info
-        self.gray_skill_info = gray_skill_info
+        self.rank_2_skill_info = rank_2_skill_info
+        self.rank_1_skill_info = rank_1_skill_info
         heroes_list.append(self)
 
     def __str__(self):
@@ -33,7 +33,8 @@ class Hero:
 
     def base_attack(self, enemy):
         if self.permissions == "dead":
-            print("You are dead. Request assistance from another hero with the power of reviving. (SAY THE MESSAGE IN COMMENTS)")
+            print(
+                "You are dead. Request assistance from another hero with the power of reviving. (SAY THE MESSAGE IN COMMENTS)")
             wait()
             return f"{self.name} tried to attack while dead."
         if enemy.permissions == "dead":
@@ -57,32 +58,42 @@ class Hero:
                 self.energy += 20
                 return f"{self.name} dealt {self.attack} {self.base_attack_stat} damage to {enemy.name}. {enemy.name}'s health is {enemy.health} \n"
 
-    def rank_1_skill(self, enemies = None, allies = None, enemy = None, ally = None):
+    def rank_1_skill(self, enemies=None, allies=None, enemy=None, ally=None):
         if self.permissions == "dead":
             print(f"Stop it.")
             return None
-        if not self.gray_skill_info:
+        if not self.rank_1_skill_info:
             print(f"{self.name} doesn't have a Gray Skill Implemented Yet")
             return None
         if self.energy >= 150:
             self.energy -= 150
-            if self.gray_skill_info["type"] == "all damage":
-                print(f"{self.name} used their gray skill {self.gray_skill_info['name']} on the enemies!")
+            if self.rank_1_skill_info["type"] == "all damage":
+                print(f"{self.name} used their gray skill {self.rank_1_skill_info['name']} on the enemies!")
                 if enemies != None:
-                    for enemy in enemies:
-                        if (enemy.health - (self.attack + 500)) <= 0:
-                            print(f"{enemy.name} is dead.")
-                            enemy.health = 0
-                            return f"{self.name} used their green skill {self.green_skill_info['name']} and dealt {(self.attack + 500) - enemy.health} damage to {enemy.name}. {enemy.name}'s health is {enemy.health}."
+                    for e in enemies:
+                        if (e.health - (self.attack + 500)) <= 0:
+                            print(f"{e.name} is dead.")
+                            e.health = 0
                         else:
-                            enemy.health -= (self.attack + 500)
+                            e.health -= (self.attack + 500)
                             print(
-                                f"{enemy.name} only has {enemy.health} health left.")
-                            return f"{self.name} used their green skill {self.green_skill_info['name']} and dealt {self.attack + 500} damage to {enemy.name}. {enemy.name}'s health is {enemy.health}."
+                                f"{e.name} only has {e.health} health left.")
                 else:
                     print("ERROR")
-
-    def rank_2_skill(self, enemy = None):
+            elif self.rank_1_skill_info["type"] == "all heal":
+                print(f"{self.name} used their gray skill {self.rank_1_skill_info['name']} on the enemies!")
+                if allies != None:
+                    for a in allies:
+                        if (a.health + (self.attack + 500)) >= a.max_health:
+                            print(f"{a.name} got healed by {self.attack + 500 - a.max_health}! {a.name} is now at " 
+                                  "max health.")
+                            a.health = 0
+                        else:
+                            a.health += (self.attack + 500)
+                            print(f"{a.name} got healed by {self.attack + 500}! {a.name}'s health is now at {a.health}")
+                else:
+                    print("ERROR")
+    def rank_2_skill(self, enemy=None):
         if self.permissions == "dead":
             print(f"Stop it.")
             return None
@@ -106,7 +117,7 @@ class Hero:
                         f"{enemy.name} only has {enemy.health} health left.")
                     return f"{self.name} used their green skill {self.rank_2_skill_info['name']} and dealt {self.attack + 300} damage to {enemy.name}. {enemy.name}'s health is {enemy.health}."
 
-            if self.green_skill_info['type'] == "self heal":
+            if self.rank_2_skill_info['type'] == "self heal":
                 if (self.health + 350) > self.max_health:
                     print(
                         f"{self.name} used their green skill {self.rank_2_skill_info['name']} and healed themselves by {(self.health + 350) - self.max_health}. \n"
